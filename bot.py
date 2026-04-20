c981adaea4e883c0f63f0eb9a41dc988a4dca521
Its Sahil Time <vermaji23237@gmail.com>
2026-04-20 14:49:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ async def about(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def thanks(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Thank you—that was my duty. 😎")
 
-app = ApplicationBuilder().token(8771929884:AAGx1OITXVv_-kqZpL5-AVn1rHvK8kyia8c).build()
+app = ApplicationBuilder().token("8771929884:AAGx1OITXVv_-kqZpL5-AVn1rHvK8kyia8c").build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("about", about))
 app.add_handler(CommandHandler("thanks", thanks))
