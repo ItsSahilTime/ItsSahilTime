c554fc634b2598097cef8d7f6f3b97d8e961dbd4
Its Sahil Time <vermaji23237@gmail.com>
2026-04-20 14:45:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
@@ -12,7 +13,7 @@ async def about(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def thanks(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Thank you—that was my duty. 😎")
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
+app = ApplicationBuilder().token(8771929884:AAGx1OITXVv_-kqZpL5-AVn1rHvK8kyia8c).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("about", about))
 app.add_handler(CommandHandler("thanks", thanks))
