6acb17c54f909b4806ab12195f71a9a795b0c67f
Its Sahil Time <vermaji23237@gmail.com>
2026-04-20 14:55:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,44 @@
-import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler
 
-BOT_TOKEN = "8771929884:AAGx1OITXVv_-kqZpL5-AVn1rHvK8kyia8c"
-
+# START COMMAND
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello👋, How Are You ? 🚀")
+    keyboard = [
+        [InlineKeyboardButton("About ℹ️", callback_data="about")],
+        [InlineKeyboardButton("Help 📖", callback_data="help")],
+        [InlineKeyboardButton("Thanks ❤️", callback_data="thanks")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
 
+    await update.message.reply_text("Hello 👋 Choose an option:", reply_markup=reply_markup)
+
+# BUTTON CLICK HANDLE
+async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+
+    if query.data == "about":
+        await query.edit_message_text("This is my own Bot 😎")
+
+    elif query.data == "help":
+        await query.edit_message_text("Use commands:\n/start\n/about\n/thanks")
+
+    elif query.data == "thanks":
+        await query.edit_message_text("Thank you—that was my duty 😎")
+
+# COMMANDS
 async def about(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("This is my own Bot. 😎")
+    await update.message.reply_text("This is my own Bot 😎")
 
 async def thanks(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Thank you—that was my duty. 😎")
+    await update.message.reply_text("Thank you—that was my duty 😎")
 
+# APP
 app = ApplicationBuilder().token("8771929884:AAGx1OITXVv_-kqZpL5-AVn1rHvK8kyia8c").build()
+
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("about", about))
 app.add_handler(CommandHandler("thanks", thanks))
+app.add_handler(CallbackQueryHandler(button_handler))
 
 app.run_polling()
